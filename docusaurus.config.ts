f4ee01e086a9dfcf8aeb64e48da5cea045e3db81
Lorenzo Scebba <lorenzo.scebba@intre.it>
2024-05-29 11:23:33 +0200

    Minor fixes

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,54 +1,56 @@
-import { themes as prismThemes } from 'prism-react-renderer';
-import type { Config } from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: 'D&DDD',
-  tagline: 'Dungeons & Dragons Driven Development',
-  favicon: 'img/logo.png',
+  title: "D&DDD",
+  tagline: "Dungeons & Dragons Driven Development",
+  favicon: "img/logo.png",
 
   // Set the production url of your site here
-  url: 'https://dddd.lorenzoscebba.com',
+  url: "https://dddd.dndumb.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'LorenzoScebba', // Usually your GitHub org/user name.
-  projectName: 'dddd', // Usually your repo name.
+  organizationName: "dndumb", // Usually your GitHub org/user name.
+  projectName: "dddd", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en", "it"],
+    localeConfigs: {
+      en: {
+        label: "English",
+        htmlLang: "en-GB",
+      },
+      it: {
+        label: "Italiano",
+        htmlLang: "it-IT",
+      },
+    },
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          sidebarPath: "./sidebars.ts",
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -56,21 +58,21 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: 'D&DDD',
+      title: "D&DDD",
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.png',
+        alt: "My Site Logo",
+        src: "img/logo.png",
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Tutorial",
         },
-        { to: '/blog', label: 'Blog', position: 'left' }
+        { to: "/blog", label: "Blog", position: "left" },
       ],
     },
     prism: {
